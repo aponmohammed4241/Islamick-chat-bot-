c6a1d28df6b4e6d03046b700f40149cd26d4e9bc
aponmohammed42 <mphone1246@gmail.com>
2025-04-24 07:28:00 -0700

    Update ｢File｣__adminnoti.js

--- Script/commands/｢File｣__adminnoti.js
+++ Script/commands/｢File｣__adminnoti.js
@@ -5,7 +5,7 @@ module.exports.config = {
     name: "adminnoti",
     version: "1.0.0",
     hasPermission: 2,
-    credits: "𝐈𝐬𝐥𝐚𝐦𝐢𝐜𝐤 𝐂𝐲𝐛𝐞𝐫",
+    credits: "my master apon",
     description: "",
     commandCategory: "admin",
     usages: "[msg]",
