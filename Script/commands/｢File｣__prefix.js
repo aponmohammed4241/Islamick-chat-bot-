7f8626611e3170eccb973ca3959341afc2d0a067
aponmohammed42 <mphone1246@gmail.com>
2025-04-24 07:37:11 -0700

    Update ｢File｣__prefix.js

--- Script/commands/｢File｣__prefix.js
+++ Script/commands/｢File｣__prefix.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "prefix",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "my master apon",
   description: "given prefix detail",
   commandCategory: "Dành cho Admin",
   usages: "",
